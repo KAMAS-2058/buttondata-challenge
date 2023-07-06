ca59145e8838e7e3d31c30aca043402193e5691d
Kyle <kyle.stephens@ucdenver.edu>
2023-07-06 14:30:06 -0600

    setup

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+
+const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
+
+// making api call to url provided
+d3.json(samples.json).then( sampledata =>{
+    
+})
