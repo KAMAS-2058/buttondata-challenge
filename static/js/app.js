4678a13d4d7f5efd24bc6b42d33ec9aef108ca27
Kyle <kyle.stephens@ucdenver.edu>
2023-07-07 18:15:01 -0600

    trace

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,18 @@
 
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
 
-// making api call to url provided
-d3.json(samples.json).then( sampledata =>{
+// 
+console.log(samples);
+
+sample_data = samples.map(function (row){
+    return row
+});
+
+let top_ten = 
+
+let trace1 = {
+    x: 
+    y:
     
-})
+};
+
